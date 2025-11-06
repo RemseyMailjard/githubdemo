7e21c069a9a5fd7d67abf0c762cd7054a1fd78b9
Remsey Mailjard <remsey@skills4-it.nl>
2025-11-06 20:21:24 +0200

    Oops I removed 3 lines

--- src/main/java/nl/skills4it/gitrecovery/App.java
+++ src/main/java/nl/skills4it/gitrecovery/App.java
@@ -10,7 +10,7 @@ public class App {
 
     public static void main(String[] args) {
 
-
+        System.out.println("another bug");
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
